7925a6f3d9b2d0291f559c77fb16a6659aa924ad
Aashiqui <ashikmail2747@gmail.com>
2024-08-30 14:54:09 +0530

    delete added

--- SingleLinkedList/SingleLinkedListDemo.java
+++ SingleLinkedList/SingleLinkedListDemo.java
@@ -39,35 +39,75 @@ public class SingleLinkedListDemo {
     }
 
     public void insertAtPos(int val, int pos) {
-        Node newnode=new Node(val);
-        if (pos == 0)
-        {
+        Node newnode = new Node(val);
+        if (pos == 0) {
             insertAtBegin(val);
             return;
-
         }
-        if(head==null)
-        {
+        if (head == null) {
             System.out.println("Unable to insert list is empty");
             return;
-        }
-        else if(head.next==null)
-        {
-            head.next=newnode;
-        }
-        else{
-            Node temp=head;
-            for(int i=0;i<pos-1;i++)
-            {
-                temp=temp.next;
+        } else if (head.next == null) {
+            head.next = newnode;
+        } else {
+            Node temp = head;
+            for (int i = 0; i < pos - 1; i++) {
+                temp = temp.next;
             }
-            if(temp==null)
-            {
+            if (temp == null) {
                 System.out.println("invalid Position");
                 return;
             }
-            newnode.next=temp.next;
-            temp.next=newnode;
+            newnode.next = temp.next;
+            temp.next = newnode;
+        }
+    }
+
+    public void deleteAtBegin() {
+        if (head == null) {
+
+            System.out.println("List is empty");
+            return;
+        } else {
+            head = head.next;
+        }
+    }
+
+    public void deleteAtEnd() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        } else if (head.next == null) {
+            head = head.next;
+        } else {
+            Node temp = head;
+            while (temp.next.next != null) {
+                temp = temp.next;
+            }
+            temp.next = null;
+        }
+    }
+
+    public void deleteAtPos(int pos) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        } else if (pos == 0) {
+            deleteAtBegin();
+            return;
+        } else {
+            Node temp = head;
+            Node prev = null;
+            for (int i = 0; i < pos; i++) {
+                prev = temp;
+                temp = temp.next;
+            }
+            if(temp==null)
+            {
+                System.out.println("Unable tp delete in the pos");
+                return;
+            }
+            prev.next = temp.next;
         }
     }
 
@@ -82,11 +122,14 @@ public class SingleLinkedListDemo {
 
     public static void main(String[] args) {
         SingleLinkedListDemo ll = new SingleLinkedListDemo();
-        // ll.insertAtBegin(3);
-        // ll.insertAtBegin(2);
-        // ll.insertAtBegin(1);
-        // ll.insertAtEnd(4);
-        ll.insertAtPos(5, 5);
+        ll.insertAtBegin(3);
+        ll.insertAtBegin(2);
+        ll.insertAtBegin(1);
+        ll.insertAtEnd(4);
+        ll.insertAtPos(5, 4);
+        ll.deleteAtBegin();
+        ll.deleteAtEnd();
+        ll.deleteAtPos(3);
         ll.display();
     }
 }
